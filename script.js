75c03ee8fe569cc77d70a8adb80ac01bf657b739
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-08-18 08:56:51 +0530

    Added Close button  after hamburger clicked under 760 px screens

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const hamburger = document.querySelector('.hamburger');
 const navButtons = document.querySelector('.navbar-buttons');
+const close = document.querySelector('.close-btn');
+const body = document.body;
 
 hamburger.addEventListener('click', () => {
-  navButtons.classList.toggle('active');
+  navButtons.classList.toggle("active");
+  body.classList.add("active");
+});
+
+close.addEventListener('click', () => {
+  navButtons.classList.remove("active");
+  body.classList.remove("active");
 });

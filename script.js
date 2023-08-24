310a365cec92805062a3a1b28bde98b9e0e1fa2a
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-08-24 09:13:34 +0530

    Add scroll feature while click on comp name in footer

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.hamburger');
 const navButtons = document.querySelector('.navbar-buttons');
 const close = document.querySelector('.close-btn');
 const body = document.body;
+const scroll = document.querySelector('.Scrolls');
 
 hamburger.addEventListener('click', () => {
   navButtons.classList.toggle("active");
@@ -12,3 +13,15 @@ close.addEventListener('click', () => {
   navButtons.classList.remove("active");
   body.classList.remove("active");
 });
+
+scroll.addEventListener('click', ()=>{
+  top1();
+  });
+
+  function top1(){
+
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth" // You can use "auto" for instant scrolling
+  });
+}

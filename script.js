7222b3d6fe152579e538b0579fbec36e74d7179f
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-08-25 10:20:57 +0530

    Formatted files with prettier extension

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-const hamburger = document.querySelector('.hamburger');
-const navButtons = document.querySelector('.ham-nav');
-const close = document.querySelector('.close-btn');
+const hamburger = document.querySelector(".hamburger");
+const navButtons = document.querySelector(".ham-nav");
+const close = document.querySelector(".close-btn");
 const body = document.body;
-const scroll = document.querySelector('.Scrolls');
+const scroll = document.querySelector(".Scrolls");
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener("click", () => {
   navButtons.classList.toggle("active");
   body.classList.add("active");
 });
 
-close.addEventListener('click', () => {
+close.addEventListener("click", () => {
   navButtons.classList.remove("active");
   body.classList.remove("active");
 });
 
-scroll.addEventListener('click', ()=>{
+scroll.addEventListener("click", () => {
   top1();
-  });
+});
 
-  function top1(){
-
+function top1() {
   window.scrollTo({
     top: 0,
-    behavior: "smooth" // You can use "auto" for instant scrolling
+    behavior: "smooth", // You can use "auto" for instant scrolling
   });
 }
